1c4630a1448b81fedcd66d51df5c77d6b02f8c30
Wassim-Elma2005 <wassim-elma@hotmail.com>
2025-05-08 14:07:15 +0200

    Log bestand aangemaakt
    In de code wordt er nu een logbestand aangemaakt, en als er een error is wordt die hier netjes opgeslagen met de datum. ik kwam hier ook achter dat ik een typ fout had gemaakt, deze heb ik ook verbeterd bij de error check. (if err != nil)

--- main.go
+++ main.go
@@ -5,14 +5,27 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"log"
 )
 
 func main() {
+
 	//file, _ := os.ReadFile("kleuren.json")
 	// _ Negeer eventuele fouten. programma stopt gewoon als het crasht
 	//betere versie
+
+	//logbestand openen of aanmaken
+	logfile, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("kon logbestand niet openen", err)
+		fmt.Println("Kon logbestand niet openen", err)
+		return
+	}
+	log.SetOutput(logfile)
+
 	file, err := os.ReadFile("Kleuren.json")
-	if err != nill {
+	if err != nil {
+		log.Println("fout bij het lezen van kleuren.json", err)
 		fmt.Println("Fout bij het lezen van kleuren.json", err)
 		return
 	}
@@ -33,9 +46,12 @@ func main() {
 	if gevonden {
 		os.WriteFile("output.txt", []byte(tekst), 0644)
 		fmt.Println(tekst)
+		log.Println("Kleur herkend: ", input)
+		
 	} else {
 		foutmelding := "Onbekende kleur ingevoerd: " + input
 		os.WriteFile("output.txt", []byte(foutmelding), 0644)
+		log.Println(foutmelding)
 		fmt.Println(foutmelding)
 	}
 }
